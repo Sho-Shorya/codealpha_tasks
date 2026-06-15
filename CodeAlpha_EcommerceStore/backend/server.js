d08e5710afa7fd2d15a6c7c78b92d786fae0ea05
unknown <shoryachhabra308@gmail.com>
2026-06-15 15:20:59 +0530

    updates2

--- CodeAlpha_EcommerceStore/backend/server.js
+++ CodeAlpha_EcommerceStore/backend/server.js
@@ -1,9 +1,9 @@
 import express from "express"
 import 'dotenv/config'
-import connectDb from "./database/db.js";
+import { connectDb } from "./database/db.js";
 import userRoute from "./routes/userRoute.js"
 import productRoute from "./routes/productRoute.js"
-import cors from'cors'
+import cors from 'cors'
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials:true
+  credentials: true
 }))
 
 app.use('/api/v1/user', userRoute)
@@ -21,7 +21,7 @@ app.use('/api/v1/product', productRoute)
 
 // http://localhost:8000/api/v1/user/register
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   connectDb()
   console.log("Server started successfully");
   console.log(`Server running on port ${PORT}`);
