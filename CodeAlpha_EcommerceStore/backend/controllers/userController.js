ff91ba664dc7c735dae235a0d7eb99ab24d215ac
unknown <shoryachhabra308@gmail.com>
2026-06-15 18:49:22 +0530

    updates6

--- CodeAlpha_EcommerceStore/backend/controllers/userController.js
+++ CodeAlpha_EcommerceStore/backend/controllers/userController.js
@@ -70,7 +70,7 @@ export const verify = async (req, res) => {
     if (!user) {
       return res.status(400).json({ success: false, message: "User not found!" })
     }
-    user.token = null;
+    // user.token = null;
     user.isVerified = true;
     await user.save()
     return res.status(200).json({ success: true, message: "Email verified successfully" })
