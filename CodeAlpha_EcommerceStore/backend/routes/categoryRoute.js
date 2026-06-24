92045aa45bc9caa00f80f7403d6b5840a12e5a93
unknown <shoryachhabra308@gmail.com>
2026-06-24 19:22:22 +0530

    category Updates2

--- CodeAlpha_EcommerceStore/backend/routes/categoryRoute.js
+++ CodeAlpha_EcommerceStore/backend/routes/categoryRoute.js
@@ -8,6 +8,6 @@ router.get("/", listCategories)
 router.get("/get-cat/:id", getCategory)
 router.post("/create-cat", isAuthenticated, isAdmin, createCategory)
 router.put("/update-cat/:id", isAuthenticated, isAdmin, updateCategory)
-router.delete("/delete-cat:id", isAuthenticated, isAdmin, deleteCategory)
+router.delete("/delete-cat/:id", isAuthenticated, isAdmin, deleteCategory)
 
 export default router
